af3931ff23503d8aedef4dbf79a5c5003a852e45
Gareth Brickman <977@holbertonschool.com>
2020-01-05 22:11:13 +0000

    [ADD] 3.

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Fabric script to call do_deploy() and do_pack() methods
+"""
+import os
+import time
+from fabric.api import *
+from pathlib import Path
+
+env.user = 'ubuntu'
+env.hosts = ['35.243.219.137', '35.237.245.133']
+
+
+def do_pack():
+    """ Archives web_static contents as a tarball to /versions folder
+    """
+    date_var = time.strftime("%Y%m%d%H%M%S")
+    directory = "versions/web_static_{}.tgz".format(date_var)
+
+    try:
+        local("mkdir -p ./versions && tar -zcvf ./{} ./web_static"
+              .format(directory))
+    except:
+        return None
+    else:
+        return directory
+
+
+def do_deploy(archive_path):
+    """ Uploads archive to /tmp/, uncompresses, cleans up
+    """
+    p = Path(archive_path)
+    f_full = p.name
+    f_noext = p.stem
+
+    if not os.path.exists(archive_path):
+        return False
+    else:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(f_noext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(f_full, f_noext))
+        run("rm /tmp/{}".format(f_full))
+        run("mv /data/web_static/releases/{}/web_static/*\
+    /data/web_static/releases/{}/".format(f_noext, f_noext))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(f_noext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(f_noext))
+        return True
+
+    @decorators.runs_once
+    def deploy():
+        """ Calls do_pack() and do_deploy() methods
+        """
+        try:
+            pack_dir = execute(do_pack)
+        except:
+            return False
+
+        deploy_ret = execute(do_deploy(pack_dir))
+        return deploy_ret
