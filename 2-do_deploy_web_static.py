3ddb97dbaa81d1336f274289f26a42eb0282afd5
Gareth Brickman <977@holbertonschool.com>
2020-01-05 21:37:37 +0000

    [TASK] Include return True at end of operations

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,15 +19,16 @@ def do_deploy(archive_path):
 
     if not os.path.exists(archive_path):
         return False
-
-    put(archive_path, "/tmp/")
-    run("mkdir -p /data/web_static/releases/{}/".format(f_noext))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(f_full, f_noext))
-    run("rm /tmp/{}".format(f_full))
-    run("mv /data/web_static/releases/{}/web_static/*\
+    else:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(f_noext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(f_full, f_noext))
+        run("rm /tmp/{}".format(f_full))
+        run("mv /data/web_static/releases/{}/web_static/*\
     /data/web_static/releases/{}/".format(f_noext, f_noext))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(f_noext))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(f_noext))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(f_noext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(f_noext))
+        return True
