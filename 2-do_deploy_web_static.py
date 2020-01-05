c336ade13a5fba330478e6d7a476cfb0d0f1d81d
Gareth Brickman <977@holbertonschool.com>
2020-01-05 21:36:08 +0000

    [TASK] Fixed typos in filenames

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Fabric script deploys .tgz archive to servers
 """
+import os
 from fabric.api import *
-from fabric.contrib import files
 from pathlib import Path
 
-env.use_ssh_config = True
+# env.use_ssh_config = True
 env.user = 'ubuntu'
 env.hosts = ['35.243.219.137', '35.237.245.133']
 
@@ -17,16 +17,16 @@ def do_deploy(archive_path):
     f_full = p.name
     f_noext = p.stem
 
-    if not files.exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
 
-    put("archive_path", "/tmp")
-    run("mkdir -p /data/web_static/releases/{}".format(f_full))
+    put(archive_path, "/tmp/")
+    run("mkdir -p /data/web_static/releases/{}/".format(f_noext))
     run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
         .format(f_full, f_noext))
     run("rm /tmp/{}".format(f_full))
     run("mv /data/web_static/releases/{}/web_static/*\
-    /data/web_static/releases/{}/".format(f_noext))
+    /data/web_static/releases/{}/".format(f_noext, f_noext))
     run("rm -rf /data/web_static/releases/{}/web_static".format(f_noext))
     run("rm -rf /data/web_static/current")
     run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
