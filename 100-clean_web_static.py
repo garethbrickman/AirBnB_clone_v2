9e8beefcf2e394a5288ed73578841bdf73c2d812
Gareth Brickman <977@holbertonschool.com>
2020-01-05 23:00:51 +0000

    [ADD] 100.

---
+++ 100-clean_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Fabric script to clean up .tgz archives
+"""
+from fabric.api import *
+
+env.user = 'ubuntu'
+env.hosts = ['35.243.219.137', '35.237.245.133']
+
+
+def do_clean(number=0):
+    """ Removes .tgz files, all but number of most recent files
+    """
+
+    with cd("/data/web_static/releases"):
+        run("rm `ls -td *.tgz | awk 'NR>{}'`".format(number))
+
+    with lcd("/versions"):
+        local("rm `ls -td *.tgz | awk 'NR>{}'`".format(number))
