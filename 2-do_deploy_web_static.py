93779fbe23c295771c445f5bfb446e2b07e73a37
Gareth Brickman <977@holbertonschool.com>
2020-01-05 20:42:32 +0000

    [TASK] Use pathlib for filenames, check if file exists first

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
 """ Fabric script deploys .tgz archive to servers
 """
-import os
-from pathlib import Path
 from fabric.api import *
+from fabric.contrib import files
+from pathlib import Path
+
+env.use_ssh_config = True
+env.user = 'ubuntu'
+env.hosts = ['35.243.219.137', '35.237.245.133']
 
 
 def do_deploy(archive_path):
     """ Uploads archive to /tmp/, uncompresses, cleans up
     """
-    env.user = 'ubuntu'
-    env.hosts = [
-        '35.243.219.137',
-        '35.237.245.133'
-        ]
-    filename, file_extension = os.path.splitext(archive_path)
     p = Path(archive_path)
-    try:
-        put("archive_path", "/tmp")
-        run("mkdir -p /data/web_static/releases/{}".format(filename))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(p.name, filename))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(filename, filename))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
-    except:
+    f_full = p.name
+    f_noext = p.stem
+
+    if not files.exists(archive_path):
         return False
-    else:
-        return True
+
+    put("archive_path", "/tmp")
+    run("mkdir -p /data/web_static/releases/{}".format(f_full))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        .format(f_full, f_noext))
+    run("rm /tmp/{}".format(f_full))
+    run("mv /data/web_static/releases/{}/web_static/*\
+    /data/web_static/releases/{}/".format(f_noext))
+    run("rm -rf /data/web_static/releases/{}/web_static".format(f_noext))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(f_noext))
