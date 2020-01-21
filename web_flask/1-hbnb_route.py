16f04a412bd09d5b1150e1e73c70a8ba5b7f5629
Gareth Brickman <977@holbertonschool.com>
2020-01-21 19:29:05 +0000

    [ADD] 1.

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Flask script starts web application
+    Listening on 0.0.0.0:5000
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def display_hello():
+    """ Returns string when specified app.route is queried
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def display_hbnb():
+    """ Returns string when specified app.route is queried
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
