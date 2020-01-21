085658f4fe03f187c54f600dcfee94cae2a971f4
Gareth Brickman <977@holbertonschool.com>
2020-01-21 19:42:36 +0000

    [ADD] 2.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Flask script starts web application
+    Listening on 0.0.0.0:5000
+"""
+from flask import Flask, escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/c/<text>')
+def display_c(text):
+    """ Returns string with text variable when specified app.route is queried
+    """
+    return 'C %s' % escape(text.replace("_", " "))
+
+@app.route('/')
+def display_hello():
+    """ Returns string when specified app.route is queried
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def display_hbnb():
+    """ Returns string when specified app.route is queried
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
