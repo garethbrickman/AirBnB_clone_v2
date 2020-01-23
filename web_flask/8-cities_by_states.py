f4f40b7a06f9f2f2014beb5ff8c27fc6b47afc06
Gareth Brickman <977@holbertonschool.com>
2020-01-23 02:28:25 +0000

    [ADD] 8. py & html

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Flask script starts web application
+    Listening on 0.0.0.0:5000
+    Fetches data from FileStorage, creates dynamic HTML page, closes db session
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def render_cities_states_page():
+    """ Returns HTML page filled with data from FileStorage engine
+    """
+    states_list = []
+    objs = storage.all()
+    for key, value in objs.items():
+        if key.split(".")[0] == "State":
+                states_list.append(value)
+    return render_template('8-cities_by_states.html', states=states_list)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """ Closes database session
+    """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
